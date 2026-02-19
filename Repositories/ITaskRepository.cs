eecc7ee3e11f0d28714e4ac716d351c658818909
Viktor <a0000@MacBook-Air-0000.local>
2026-02-20 00:02:35 +0700

    написал интерфейс для репозитория, потом в самом репозитории реализовал это все + создал файлы dto в соответствующей папке

---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,15 @@
+namespace project
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+
+        Task<TaskItem?> GetByIdAsync(int id); // "?" - может быть null
+
+        Task AddAsync(TaskItem taskItem);
+
+        Task UpdateAsync(TaskItem taskItem);
+
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Repositories/TaskRepository.cs
@@ -0,0 +1,44 @@
+namespace project
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly AppDbContext _db;
+        public TaskRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllAsync()
+        {
+            return await _db.TaskItems.ToListAsync();
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(int id)
+        {
+            var taskItem = await _db.TaskItems.FirstOrDefaultAsync(t => t.Id == id);
+            return taskItem;
+        }
+
+        public async Task AddAsync(TaskItem taskItem)
+        {
+            await _db.TaskItems.AddAsync(taskItem);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TaskItem taskItem)
+        {
+            _db.TaskItems.Update(taskItem);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var task = await _db.TaskItems.FindAsync(id);
+            if (task != null)
+            {
+                _db.TaskItems.Remove(task);
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
