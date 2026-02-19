45a4f45abd87c31826413c175987b795919acf7f
Viktor <a0000@MacBook-Air-0000.local>
2026-02-19 23:30:00 +0700

    создал нижний слой (сделал модель + AppDbContext на его основе (конструктор + DbSet<TaskItem>))

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+namespace project
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace project
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        [Required] 
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty();
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
